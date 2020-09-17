0141dcf4337ee477d11970a0a46c66d00b679246
Siriwat <siriwatjaiyungyuen@gmail.com>
2020-09-17 16:24:09 +0700

    Update share link

--- routes/playgroundSave.js
+++ routes/playgroundSave.js
@@ -1,50 +1,50 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
     host: 'remotemysql.com',
     port: 3306,
     user: 'mKUsVS9x6G',
     password: 'wGN54WmUBz',
     database: 'mKUsVS9x6G'
 });
-connection.connect();
+// connection.connect();
+connection.on('error', function (err) {
+    console.log('caught this error: ' + err.toString());
+});
+function handleDisconnect(connection) {
+    connection.on('error', function (err) {
+        if (!err.fatal) {
+            return;
+        }
+        if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
+            throw err;
+        }
 
-var fs = require('fs');
+        console.log('Re-connecting lost connection: ' + err.stack);
+
+        connection = mysql.createPool(conn.config);
+        handleDisconnect(connection);
+        connection.getConnection();
+    });
+}
+handleDisconnect(connection);
+
 router.get('/:id', (req, res, next) => {
-    try {
-
-        connection.query('SELECT * FROM shared_code WHERE id = ?', [req.params.id], (err, rows) => {
-            if (err) throw err;
-            res.render('playground', { data: rows[0].data });
-            res.end();
-        });
-
-    } catch (err) {
-        // res.redirect('/')
-        console.log(err);
-        res.send(err);
+    connection.query('SELECT * FROM shared_code WHERE id = ?', [req.params.id], (err, rows) => {
+        if (err) throw err;
+        res.render('playground', { data: rows[0].data });
         res.end();
-    }
+    });
 });
 router.post('/', (req, res, next) => {
-    try {
-        let name = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 10);
-
-
-        connection.query('INSERT INTO shared_code (id, data) VALUES (?,?)', [name, req.body.data], (err) => {
-            if (err) throw err;
-            res.send('ลิ้งค์สำหรับแชร์คือ <a href="http://codex-skdw.herokuapp.com/playground/save/' + name + '" target="_blank"> http://codex-skdw.herokuapp.com/playground/save/' + name + '</a>');
-            res.end();
-
-        })
-
-        // let txt = fs.writeFileSync('./public/shared_code/' + name + '.txt', req.body.data);
-    } catch (error) {
-        console.log(err);
-        res.send('เพิ่มไม่สำเร็จ');
+    let name = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 15);
+    connection.query('INSERT INTO shared_code (id, data) VALUES (?,?)', [name, req.body.data], (err) => {
+        if (err) throw err;
+        res.send(name);
         res.end();
-    }
+    })
+
 
 });
 module.exports = router;
