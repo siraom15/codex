e6b61c3259c7fd3a4a7facf552b782987f7b2d4f
Siriwat <siriwatjaiyungyuen@gmail.com>
2020-10-26 04:35:05 +0700

    [ update] disable logger

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
